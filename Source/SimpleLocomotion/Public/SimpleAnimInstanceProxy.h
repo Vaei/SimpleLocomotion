b76121aca4bbdeab7b61ba9c5df0eebc7f916f13
Jared Taylor <jaredtaylor.99@gmail.com>
2024-06-08 11:29:37 +1200

    Check floor is walkable. Dont repeat error messages. Extract jump/fall func. Fix bug for incorrect bool.

--- Source/SimpleLocomotion/Public/SimpleAnimInstanceProxy.h
+++ Source/SimpleLocomotion/Public/SimpleAnimInstanceProxy.h
@@ -14,12 +14,33 @@ struct FSimpleAnimInstanceProxy : public FAnimInstanceProxy
 	FSimpleAnimInstanceProxy(UAnimInstance* Instance)
 		: Super(Instance)
 		, PendingMessageLogs({})
+		, UniqueMessageLogs({})
 	{}
 	
 	FSimpleAnimInstanceProxy()
 		: PendingMessageLogs({})
+		, UniqueMessageLogs({})
 	{}
+	
+	void AddPendingMessage(const FString& LogMessage, const FString& LogCategory = "PIE")
+	{
+		if (!UniqueMessageLogs.Contains(LogMessage))
+		{
+			PendingMessageLogs.Add(LogCategory, LogMessage);
+			UniqueMessageLogs.Add(LogMessage);
+		}
+	}
 
+	const TMap<FString, FString>& GetPendingMessageLogs() const { return PendingMessageLogs; }
+	
+	void ResetPendingMessageLogs() { PendingMessageLogs.Reset(); }
+	void EmptyPendingMessageLogs() { PendingMessageLogs.Empty(); }
+
+private:
 	UPROPERTY()
 	TMap<FString, FString> PendingMessageLogs;  // Log category : Log message
+
+	UPROPERTY()
+	TArray<FString> UniqueMessageLogs;  // Don't repeat already printed messages
+
 };
